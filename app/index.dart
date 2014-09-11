51198e5fec4a0ed453d9ca539569d74d86dd87fb
Markuz GJ <markuz.br@gmail.com>
2014-09-10 18:36:04 -0700

    init

---
+++ app/index.dart
@@ -0,0 +1,12 @@
+// import 'package:polymer/polymer.dart';
+// import 'package:logging/logging.dart';
+
+// main() {
+
+//   Logger.root.level = Level.ALL;
+//   Logger.root.onRecord.listen((rec) {
+//     print(rec.message);
+//   });
+
+//   initPolymer();
+// }
